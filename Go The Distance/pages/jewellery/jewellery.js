5fe94ae7ea044c04647eabe92cdf72f5904ebb5c
eight uncle <348340455@qq.com>
2017-04-21 14:42:47 +0800

    尝试的

---
+++ Go The Distance/pages/jewellery/jewellery.js
@@ -0,0 +1,14 @@
+//jewellery.js
+Page({
+  data: {
+    imgUrls: [
+      '../../image/d.jpg',
+      '../../image/e.jpg',
+      '../../image/f.jpg'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000
+  }
+})
---
+++ Go The Distance/pages/my/my.js
@@ -0,0 +1,10 @@
+//my.js
+var util = require('../../utils/util.js')
+Page({
+  data: {
+    journey: []
+  },
+  onLoad: function () {
+    
+  }
+})
